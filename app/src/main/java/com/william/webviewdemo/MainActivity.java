c00fdb3ddce76cd4832c261e48cc449f558a253d
william1002 <2656603750@qq.com>
2016-08-20 20:46:18 +0800

    Initial commit

---
+++ app/src/main/java/com/william/webviewdemo/MainActivity.java
@@ -0,0 +1,38 @@
+package com.william.webviewdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = (WebView)findViewById(R.id.webView1);
+        initSetting();
+        initView();
+    }
+    void initSetting(){
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);//设置js可以直接打开窗口，如window.open()，默认为false
+        webView.getSettings().setJavaScriptEnabled(true);//是否允许执行js，默认为false。设置true时，会提醒可能造成XSS漏洞
+        webView.getSettings().setSupportZoom(true);//是否可以缩放，默认true
+        webView.getSettings().setBuiltInZoomControls(true);//是否显示缩放按钮，默认false
+        webView.getSettings().setUseWideViewPort(true);//设置此属性，可任意比例缩放。大视图模式
+        webView.getSettings().setLoadWithOverviewMode(true);//和setUseWideViewPort(true)一起解决网页自适应问题
+        webView.getSettings().setAppCacheEnabled(true);//是否使用缓存
+        webView.getSettings().setDomStorageEnabled(true);//DOM Storage
+    }
+    void initView(){
+        webView.loadUrl("http://www.baidu.com/");
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+    }
+}
